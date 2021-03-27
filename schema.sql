3e8de669817eeeae63dadf327719ec0ecc88d9c6
KB Desktop <Keithblack4290gmail.com>
2021-03-27 17:22:53 -0600

    schema, packages, server. js

---
+++ schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS company_db;
+
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE employees (
+id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INTEGER NOT NULL,
+manager_id INTEGER NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INTEGER NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE departments (
+id INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(30) NOT NULL,
+PRIMARY KEY (id)
+);
+
+ALTER TABLE departments AUTO_INCREMENT = 1001;
+
